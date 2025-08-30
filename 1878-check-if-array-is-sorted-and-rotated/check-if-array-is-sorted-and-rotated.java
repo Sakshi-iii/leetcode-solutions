43192bcd34d09fdf04c4c7cb8d6d0cd4f3845f3a
Sakshi <guptasakshi0002003@gmail.Com>
2025-08-30 14:00:16 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (82.74%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,20 +1,19 @@
 class Solution {
     public boolean check(int[] nums) {
-        int count=0;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]>nums[i+1]){
-                count++;
-            }
-        }
-        if(nums[nums.length-1]>nums[0]){
+       int count=0;
+       for(int i=0;i<nums.length-1;i++){
+        if(nums[i]>nums[i+1]){
             count++;
         }
-        if(count<=1){
-            return true;
-        }
-        else{
-            return false;
-        }
+       }//basically if given array will be exactly in non decreasing and rotating then count can be 0 or 1, 
+       if(nums[nums.length-1]>nums[0]){
+        count++;
+       }
+       if(count<=1){
+        return true;
+       }else{
+        return false;
+       }
     
     } 
 }
