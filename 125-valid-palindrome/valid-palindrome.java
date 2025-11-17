d42e061414a76b2f0559bf068d44872c80e8059b
Sakshi <guptasakshi0002003@gmail.Com>
2025-11-17 10:47:01 +0530

    Time: 2 ms (98.53%) | Memory: 44.1 MB (54.68%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // two pointer
+        //use two pointer
         int left=0;
         int right=s.length()-1;
         while(left<right){
@@ -10,8 +10,7 @@ class Solution {
              while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
                 right--;
             }
-            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right)))
-            {
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
                 return false;
             }
             left++;
